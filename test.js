d64ef92bd85febe3782ebe1d94e40e65d7212f06
Randy Tarampi <rt@randytarampi.ca>
2019-03-10 10:36:04 -0700

    chore(package): Update dependencies.

--- test.js
+++ test.js
@@ -12,28 +12,28 @@ test('returns all icons in array', function (t) {
 
 test('returns icon for size 48 as Number', function (t) {
   t.plan(2)
-  var icon = androidIcons({size: 48})
+  var icon = androidIcons({ size: 48 })
   t.ok(icon.name === 'mdpi.png', 'icon name correct')
   t.ok(icon.width === 48, 'icon width correct')
 })
 
 test('returns icon for size 48 as String', function (t) {
   t.plan(2)
-  var icon = androidIcons({size: '48'})
+  var icon = androidIcons({ size: '48' })
   t.ok(icon.name === 'mdpi.png', 'icon name correct')
   t.ok(icon.width === 48, 'icon width correct')
 })
 
 test('returns icon for size 96', function (t) {
   t.plan(2)
-  var icon = androidIcons({size: 96})
+  var icon = androidIcons({ size: 96 })
   t.ok(icon.name === 'xhdpi.png', 'icon name correct')
   t.ok(icon.width === 96, 'icon width correct')
 })
 
 test('returns icon for size xhdpi', function (t) {
   t.plan(2)
-  var icon = androidIcons({size: 'xhdpi'})
+  var icon = androidIcons({ size: 'xhdpi' })
   t.ok(icon.name === 'xhdpi.png', 'icon name correct')
   t.ok(icon.width === 96, 'icon width correct')
 })
